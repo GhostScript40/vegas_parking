69f25bed49dc0a931e33a27a9d351032f87c7b13
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-28 02:41:32 +0000

    Added methods to the CLI file.

--- lib/vegas_parking/cli.rb
+++ lib/vegas_parking/cli.rb
@@ -2,5 +2,15 @@ class VegasParking::CLI
   
   def call
     puts "Welcome to Vegas. How can I help you park today?"
+    list_spots
+    spots
   end
-end
+  
+  def list_spots
+    puts "Here is a list of all of the free parking spots at great locations!"
+  end
+  
+  def spots
+    puts "Please enter your spot selection."
+  end
+end  
