389d878f133b54a5d8610235600cce25efb62711
Tariq Bailey <tariqbailey627@gmail.com>
2019-12-03 23:31:17 +0000

    Working on the Class methods to test drive it. since now the dependency errors or fixed.

--- lib/vegas_parking/the_strip.rb
+++ lib/vegas_parking/the_strip.rb
@@ -1,10 +1,25 @@
 class VegasParking::TheStrip
   
-  attr_accessor :name, :location, :url
+  attr_accessor :name, :url, :fee, :entrance, :description
   
  def self.places
  #This should scrape the parking website
   #self.scrape_spots
+  
+  place_1 = self.new
+  place_1.name = "Harrahs"
+  place_1.entrance = "Las Vegas Blvd"
+  place_1.url = "https://www.vegas.com/hotels/harrahs2/"
+  place_1.fee = "Free"
+  place_1.description = "Very large parking garage free only to guests"
+  
+  place_2 = self.new
+  place_2.name = "Arizona Charlies"
+  place_2.entrance = "Boulder Hwy"
+  place_2.url = "https://www.vegas.com/resorts/arizonacharlieseast/"
+  place_2.fee = "Free"
+  place_2.description = "This garage has very tight turns as you go up the ramp. Be on the lookout for other drivers."
+ [place_1, place_2]
  end
   
  # def scrape_spots
