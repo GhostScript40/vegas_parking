28cfe3c7ccad9a7701ff97766f6f90c1f123b7be
Tariq Bailey <tariqbailey627@gmail.com>
2019-12-04 03:01:40 +0000

    Doing a bit of refactoring befor the scrape start

--- lib/vegas_parking/the_strip.rb
+++ lib/vegas_parking/the_strip.rb
@@ -3,30 +3,42 @@ class VegasParking::TheStrip
   
   attr_accessor :name, :url, :fee, :entrance, :description
   
-  @@all = [] 
-  
  def self.places
  #This should scrape the parking website
-  #self.scrape_spots
+  self.scrape_spots
   
-  place_1 = self.new
-  place_1.name = "Harrahs"
-  place_1.entrance = "Las Vegas Blvd"
-  place_1.url = "https://www.vegas.com/hotels/harrahs2/"
-  place_1.fee = "Free"
-  place_1.description = "Very large parking garage free only to guests"
-  
-  place_2 = self.new
-  place_2.name = "Arizona Charlies"
-  place_2.entrance = "Boulder Hwy"
-  place_2.url = "https://www.vegas.com/resorts/arizonacharlieseast/"
-  place_2.fee = "Free"
-  place_2.description = "This garage has very tight turns as you go up the ramp. Be on the lookout for other drivers."
-    [place_1, place_2]
- #binding.pry
  end
   
  def scrape_spots
-   #This method should scrape information from the URL
+   #This method should scrape URL
+   #So that I can instantiate the parking spots
+   
+   spaces = []
+   
+   spaces << self.scrape_vegas
+   
+   spaces
+   
+  #place_1 = self.new
+  #place_1.name = "Harrahs"
+  #place_1.entrance = "Las Vegas Blvd"
+  #place_1.url = "https://www.vegas.com/hotels/harrahs2/"
+  #place_1.fee = "Free"
+  #place_1.description = "Very large parking garage free only to guests"
+  
+  #place_2 = self.new
+  #place_2.name = "Arizona Charlies"
+  #place_2.entrance = "Boulder Hwy"
+  #place_2.url = "https://www.vegas.com/resorts/arizonacharlieseast/"
+  #place_2.fee = "Free"
+  #place_2.description = "This garage has very tight turns as you go up the ramp. Be on the lookout for other drivers."
+   # [place_1, place_2]
   end
+  
+  def self.scrape_vegas
+    doc = Nokogiri::HTML(open("https://www.vegas.com/transportation/free-parking-las-vegas/"))
+    binding.pry
+  end
+  
+  
 end
