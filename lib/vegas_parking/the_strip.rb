1c2860b37e6f878bbaceb0771646a88ebe810f4d
Tariq Bailey <tariqbailey627@gmail.com>
2019-12-09 06:10:21 +0000

    refactoring code

--- lib/vegas_parking/the_strip.rb
+++ lib/vegas_parking/the_strip.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class VegasParking::TheStrip
   
-  attr_accessor :name, :url, :entrance, :description
+  attr_accessor :name, :url, :description
   
  def self.places
  #This should scrape the parking website
@@ -22,31 +22,24 @@ class VegasParking::TheStrip
   def self.scrape_vegas
     
   doc = Nokogiri::HTML(open("https://www.vegas.com/transportation/free-parking-las-vegas/"))
-     names = doc.css('p').collect do |name|
-     name.css('a').text.strip
-   end
-  new_names = names.reject { |e| e.to_s.empty? }
+     names = doc.css('p').collect do |name|  ######################
+     name.css('a').text.strip                # Scrapes the names  #
+   end                                       ###################### 
+  new_names = names.reject { |e| e.to_s.empty? }  
    
      new_names.each do |name|
       name = self.new
        name.name = name
       end
+                                                                    ######################
+      links = doc.xpath('//p/a/@href').collect { |l| l.text.strip } #scrapes the links...#
+                                                                    ######################        
+      describe = doc.css('p').collect {|d| d.text} ################################
+      describe.pop()                              #                               #
+      describe.delete_at(0)                       # This scrapes the description  #
+        binding.pry                               #                               #
+                                                 ##################################    
       
-            links = doc.xpath('//p/a/@href').collect { |l| l.text.strip }
-     
-     #web_page.attribute('href').value
-       #binding.pry
-      #end
-      
-      describe = doc.css('p').collect {|d| d.text}
-      describe.pop()
-      describe.delete_at(0)
-        binding.pry
-        
-       # describe.each do |d|
-      #description = self.new
-      #name.description = description
-      #end
         
       new_names
 end
