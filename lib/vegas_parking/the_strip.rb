cc2da3d2e853abd5c30c7cdc79a7cf08c5a425fb
Tariq Bailey <tariqbailey627@gmail.com>
2019-12-11 00:06:35 +0000

    Refactored code in the_strip file

--- lib/vegas_parking/the_strip.rb
+++ lib/vegas_parking/the_strip.rb
@@ -3,11 +3,11 @@ class VegasParking::TheStrip
   
   attr_accessor :name, :url, :description
   
- def self.places
+ #def self.places
  #This should scrape the parking website
-  self.scrape_spots
+ # self.scrape_spots
   
- end
+# end
   
  def scrape_spots
    #This method should scrape URL
@@ -16,31 +16,30 @@ class VegasParking::TheStrip
    spaces = []
    
     spaces << self.scrape_vegas
+      
       spaces
  end
   
   def self.scrape_vegas
     
   doc = Nokogiri::HTML(open("https://www.vegas.com/transportation/free-parking-las-vegas/"))
-     names = doc.css('p').collect do |name|  ######################
-     name.css('a').text.strip                # Scrapes the names  #
-   end                                       ###################### 
-  new_names = names.reject { |e| e.to_s.empty? }  
+
+     names = doc.css('p').collect  {|name| name.css('a').text.strip} 
+                                              ######################
+     names.delete_at(0)                       # Scrapes the names  #
+     names.pop()                              ###################### 
+  
    
-     new_names.each do |name|
-      name = self.new
-       name.name = name
-      end
-                                                                    ######################
-      links = doc.xpath('//p/a/@href').collect { |l| l.text.strip } #scrapes the links...#
-                                                                    ######################        
+                                                                        ######################
+      links = doc.xpath('//p/a/@href').collect { |l| l.text.strip }     #scrapes the links...#
+                                                                        ######################        
+      
+      
       describe = doc.css('p').collect {|d| d.text} ################################
       describe.pop()                              #                               #
       describe.delete_at(0)                       # This scrapes the description  #
         binding.pry                               #                               #
                                                  ##################################    
       
-        
-      new_names
-end
+  end
 end
